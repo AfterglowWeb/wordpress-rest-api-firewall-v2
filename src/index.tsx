e892e0ffd4a24c9195f6d3b519d0307de4ac1702
Afterglow <cedric@moriskelly.com>
2026-06-13 18:14:23 +0200

    routes policy component

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,8 @@ import { createRoot } from '@wordpress/element';
 import App from './App';
 import AppTheme from './AppTheme';
 
-import { AdminDataProvider } from './contexts/AdminDataContext';
-import { DocumentationProvider } from './contexts/DocumentationContext';
-
-declare global {
-	interface Window {
-		restApiFirewallAdminData?: Record<string, unknown>;
-		restApiFirewallPro?: Record<string, unknown>;
-	}
-}
+import { AdminDataProvider, type AdminData } from '@contexts/AdminDataContext';
+import { DocumentationProvider } from '@contexts/DocumentationContext';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const container = document.getElementById(
@@ -22,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		return;
 	}
 
-	const adminData = window.restApiFirewallAdminData ?? {};
+	const adminData:AdminData | undefined = window.restApiFirewallAdminData;
 
 	createRoot(container).render(
 		<AdminDataProvider adminData={adminData}>
